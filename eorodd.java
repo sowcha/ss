ad731328a08cd79ef166fd95f00b231a5e5a02bc
sowcha <32437738+sowcha@users.noreply.github.com>
2017-12-14 20:01:43 +0530

    Update and rename eorod.java to eorodd.java

---
+++ eorodd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class eorodd
+{
+ public static void main(String args[])
+{
+int n;
+Scanner in=new Scanner(System.in);
+in=n.nextInt();
+if(n%2==0)
+  System.out.println("even");
+else
+   System.out.println("odd");
+}
+}
