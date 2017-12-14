029034bebacb4d53f16fb466dbe9b88373e01de5
sowcha <32437738+sowcha@users.noreply.github.com>
2017-12-14 19:20:10 +0530

    Create eorod.java

---
+++ eorod.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+class eorod
+{
+ public static void main(string args[])
+{
+int n;
+Scanner n=new Scanner(system.in);
+if(n%2==0)
+  System.out.println("even");
+else
+   System.out.println("odd");
+}
+}
