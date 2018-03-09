443b83ee26387741e411154c99755263ad74031d
sowcha <32437738+sowcha@users.noreply.github.com>
2018-03-09 12:15:19 +0530

    Add files via upload

---
+++ ALpha.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+int ch;
+
+printf("\n enter any character");
+
+scanf("%s",&ch);
+
+if((ch>='a' && ch<='z')||(ch>='A' && ch<='Z'))
+
+{
+
+ printf("\n Alphabet");
+
+}
+
+else
+
+{
+ 
+printf("\n not an alphabet");
+
+}
+
+return 0;
+
+}
